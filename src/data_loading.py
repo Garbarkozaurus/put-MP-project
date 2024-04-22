6ce83e2a10595f8c334914004a888af140bb7fbd
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2024-04-22 20:59:00 +0200

    Load model params, Export np to .off
    Makes load_off_file_np() return a tuple containing n_verts, n_faces,
    n_endges as its firs treturned value (in addition to the orher two).
    This was necessary to be able to fully recreate an .off file.
    This tuple is used in export_off_file_np()
    
    Accommodating changes to functions using load_off_file_np() will come in the next
    commit

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,27 +1,44 @@
 import numpy as np
 import pathlib
-from os.path import basename
 from os import rename, remove
-from typing import Literal
 
 
-def load_off_file_np(file_path: str
-                     ) -> tuple[np.ndarray[np.float32],
-                                np.ndarray[np.int32]]:
-    """First returned matrix contains vertex coordinates.
-    The second - which vertex belongs to which edge"""
+def load_off_file_np(
+        file_path: str) -> tuple[
+                        tuple[int, int, int],
+                        np.ndarray[np.float32],
+                        np.ndarray[np.int32]]:
+    """The return tuple has three elements: a three-int tuple,
+    and two np.ndarrays. The tuple stores the number of vertices,
+    faces and edges in the model (in this order - as they are given
+    in the file). The first of the matrices contains vertex coordinates.
+    The second - which vertex belongs to which face"""
     with open(file_path, "r") as file:
         if 'OFF' != file.readline().strip():
             raise ValueError('Not a valid OFF header')
         n_verts, n_faces, n_edges = tuple([int(s) for s in file.readline().strip().split(' ')])
         verts = [[float(s) for s in file.readline().strip().split(' ')] for _ in range(n_verts)]
         faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for _ in range(n_faces)]
-    return np.array(verts, dtype=np.float32), np.array(faces, np.int32)
+    return (n_verts, n_faces, n_edges), np.array(verts, dtype=np.float32), np.array(faces, np.int32)
+
+
+def export_off_file_np(
+        model_params: tuple[int, int, int],
+        vertices: np.ndarray[np.float32],
+        faces: np.ndarray[np.int32],
+        file_path: str) -> None:
+    fp = open(file_path, "w")
+    fp.write("OFF\n")
+    fp.write(" ".join(map(str, model_params))+"\n")
+    for vertex in vertices:
+        fp.write(" ".join(map(str, vertex))+"\n")
+    for face in faces:
+        fp.write(f"{len(face)} {' '.join(map(str, face))}\n")
 
 
 def iterate_over_off_models(
         glob_pattern: str,
-        dataset_root: str = "./ModelNet40/"):
+        dataset_root: str = "./ModelNet40/") -> None:
     """Example glob patterns:
     '*/*/*.off' - get all .off files
     '*/train/*.off' - get all .off files designated to teh trianing set
@@ -33,10 +50,10 @@ def iterate_over_off_models(
         load_off_file_np(file)
 
 
-def fix_invalid_files(glob_pattern: str, dataset_root: str = "./ModelNet40/"):
+def fix_invalid_files(glob_pattern: str, dataset_root: str = "./ModelNet40/") -> None:
     """Fixes the files with first line of pattern: 'OFFn_verts n_faces n_edges'
-    by replacing them with a file of the same name, and the first line split into two:
-    'OFF' and 'n_verts n_faces n_edges' """
+    by replacing them with a file of the same name, and the first line split
+    into two: 'OFF' and 'n_verts n_faces n_edges' """
     path_obj = pathlib.Path(dataset_root).glob(glob_pattern)
     for file_name in path_obj:
         fp = open(file_name, "r")
