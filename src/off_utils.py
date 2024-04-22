5d1834f0f79fac54c8af174e549923b68fc2e025
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2024-04-22 22:04:58 +0200

    Render rgbd dataset with given train/test sizes

--- src/off_utils.py
+++ src/off_utils.py
@@ -99,6 +99,73 @@ def render_offs_from_pattern(
     out_log.close()
 
 
+def export_centered_off(src_off_path: str, dest_off_path: str) -> None:
+    params, vertices, faces = data_loading.load_off_file_np(src_off_path)
+    centered_vertices = vertices - np.mean(vertices, axis=0)
+    data_loading.export_off_file_np(params, centered_vertices, faces, dest_off_path)
+
+
+def create_centered_off_dataset(
+        glob_pattern: str,
+        dataset_root: str = "ModelNet40",
+        dest_dir: str = "ModelNet40_centered") -> None:
+    """Parses all files in `dataset_root` according to glob pattern.
+    Recreates the dataset's structure in `dest_dir`, but all with models in
+    .off files shifted to have their centers of mass at (0,0,0). Translates
+    all vertices by a vector composed of the average values of x, y, z"""
+    path_obj = pathlib.Path(dataset_root).glob(glob_pattern)
+    for i, src_file_path in enumerate(path_obj):
+        out_dir = str(os.path.dirname(src_file_path)).lstrip(dataset_root)
+        out_dir = f"{dest_dir}{out_dir}/"
+        # skip if the corresponding .off if it already exsists
+        out_file_name = os.path.basename(src_file_path).split('.')[0]
+        if os.path.isfile(f"{out_dir}{out_file_name}.off"):
+            print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] SKIP {src_file_path}: {i+1}")
+            continue
+        # print(f"{out_dir}{out_file_name}")
+        pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
+        export_centered_off(src_file_path, f"{out_dir}{out_file_name}.off")
+        # there are 12311 files in the main dataset
+        print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] {src_file_path}: {i+1}")
+
+
+def render_off_dataset_set_numbers(
+        n_train: int = 64, n_test: int = 20,
+        dataset_root: str = "ModelNet40_centered",
+        dest_dir: str = "RGBD_ModelNet40_centered") -> None:
+    # no glob, because I want to be ABSOLUTELY SURE that the same order is always preserved
+    dataset_root_dir = pathlib.Path(dataset_root)
+    out_log = open("/dev/null", "w")
+    for class_dir in dataset_root_dir.iterdir():
+        class_name = os.path.basename(class_dir)
+        train_files = sorted(list(pathlib.Path(f"{class_dir}/train").iterdir()))
+        out_dir_train = f"{dest_dir}/{class_name}/train/"
+        for train_off_file in train_files[:n_train]:
+            out_file_name = os.path.basename(train_off_file).split('.')[0]
+            if os.path.isfile(f"{out_dir_train}{out_file_name}_r_000.png"):
+                print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] SKIP train/{out_file_name}")
+                continue
+            scale = scale_from_off(str(train_off_file))
+            arguments = ["blender", "--background", "--python", "./src/render_blender.py", "--",
+                f"{train_off_file}", "--output_folder", f"{out_dir_train}",
+                "--scale", f"{scale}"]
+            subprocess.run(arguments, stdout=out_log)
+        out_dir_test = f"{dest_dir}/{class_name}/test/"
+        test_files = sorted(list(pathlib.Path(f"{class_dir}/test").iterdir()))
+        for test_off_file in test_files[:n_test]:
+            out_file_name = os.path.basename(test_off_file).split('.')[0]
+            if os.path.isfile(f"{out_dir_test}{out_file_name}_r_000.png"):
+                print(f"[{datetime.datetime.now().strftime('%H:%M:%S')}] SKIP test/{out_file_name}")
+                continue
+            scale = scale_from_off(str(test_off_file))
+            arguments = ["blender", "--background", "--python", "./src/render_blender.py", "--",
+                f"{test_off_file}", "--output_folder", f"{out_dir_test}",
+                "--scale", f"{scale}"]
+            subprocess.run(arguments, stdout=out_log)
+
+
 if __name__ == "__main__":
     # create_and_render_cube(side_len=1.0)
-    render_offs_from_pattern("*/*/*.off")
+    # render_offs_from_pattern("*/*/*.off")
+    # create_centered_off_dataset("*/*/*.off")
+    render_off_dataset_set_numbers()
