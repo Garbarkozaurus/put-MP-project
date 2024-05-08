d6a49e2d3b09e312d1e73e75189319130c5d9676
Garbarkozaurus <marcin.golski@student.put.poznan.pl>
2024-05-08 10:57:58 +0200

    Function for loading RGBD data

--- src/photo_utils.py
+++ src/photo_utils.py
@@ -5,6 +5,19 @@ import matplotlib.pyplot as plt
 d_path = "RGBD_ModelNet40/airplane/test/airplane_0627_r_000_depth0001.png"
 rgb_path = "RGBD_ModelNet40/airplane/test/airplane_0627_r_000.png"
 
+
+def load_rgbd_image(rgb_png_image_path: str) -> np.ndarray[np.uint8]:
+    """Given a path to an rgb .png combines it and a corresponding depth
+    image into a sigle np.ndarray. The first three channels store RGB data,
+    and the last one - depth information.
+    """
+    d_image_path = rgb_png_image_path.rstrip(".png")+"_depth0001.png"
+    rgb_im_arr = np.array(Image.open(rgb_png_image_path))
+    d_im_arr = np.array(Image.open(d_image_path))
+    rgb_im_arr[:, :, 3] = d_im_arr  # replace alpha channel with depth data
+    return rgb_im_arr
+
+
 if __name__ == "__main__":
     d_im_arr = np.array(Image.open(d_path))
     rgb_im_arr = np.array(Image.open(rgb_path))
